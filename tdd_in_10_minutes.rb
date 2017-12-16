baab8f4e3bcd19019b75352f5ae988f156214972
Carleton DiLeo <cbdileo@gmail.com>
2017-12-16 16:19:31 -0700

    Adding testing example code

---
+++ tdd_in_10_minutes.rb
@@ -0,0 +1,38 @@
+class Calculator
+  def add(a, b)
+    a.to_i + b.to_i
+  end
+end
+
+class CalculatorTest
+  def should_add_two_values()
+    # This is the value we expect to be return by the add method
+    expected = 5
+
+    calc = Calculator.new
+    # Call the add method with two values that equal 5
+    result = calc.add(3, 2) 
+    
+    # Check if result equals the expected value
+    equal?(result, expected) 
+  end
+  # This method should be added to our CalculatorTest class
+  def should_add_two_nil_values()
+    # We need to decide what we expect the result of nil + nil is
+    expected = 0
+
+    calc = Calculator.new
+    result = calc.add(nil, nil)
+    equal?(result, expected)
+  end
+  
+  private
+  # Test if two values are equal
+  def equal?(a, b)
+    if a == b
+      puts 'PASS'
+    else
+      puts 'FAILS'
+    end
+  end
+end
